345a19fcca9a3f0bf0b623daed804b3fb87cbf31
darhf <didier.hernandez1972@gmail.com>
2025-04-28 19:56:53 -0600

    Colcon build

--- build/odometry_pkg/prefix_override/sitecustomize.py
+++ build/odometry_pkg/prefix_override/sitecustomize.py
@@ -1,8 +1,4 @@
 import sys
 if sys.prefix == '/usr':
     sys.real_prefix = sys.prefix
-<<<<<<< HEAD
-    sys.prefix = sys.exec_prefix = '/home/darhf/Manchester/Week10/odometry/install/odometry_pkg'
-=======
     sys.prefix = sys.exec_prefix = '/home/darhf/Manchester/Week10/odometry_pkg/install/odometry_pkg'
->>>>>>> d34c3ea (Cambio de paquete)
--- install/setup.sh
+++ install/setup.sh
@@ -7,11 +7,7 @@
 # since a plain shell script can't determine its own path when being sourced
 # either use the provided COLCON_CURRENT_PREFIX
 # or fall back to the build time prefix (if it exists)
-<<<<<<< HEAD
-_colcon_prefix_chain_sh_COLCON_CURRENT_PREFIX=/home/darhf/Manchester/Week10/odometry/install
-=======
 _colcon_prefix_chain_sh_COLCON_CURRENT_PREFIX=/home/darhf/Manchester/Week10/odometry_pkg/install
->>>>>>> d34c3ea (Cambio de paquete)
 if [ ! -z "$COLCON_CURRENT_PREFIX" ]; then
   _colcon_prefix_chain_sh_COLCON_CURRENT_PREFIX="$COLCON_CURRENT_PREFIX"
 elif [ ! -d "$_colcon_prefix_chain_sh_COLCON_CURRENT_PREFIX" ]; then
